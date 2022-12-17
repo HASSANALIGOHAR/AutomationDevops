5875710f8e24ae44c72c795b3507ec9bfef5ee98
HASSANALIGOHAR <sp19bsse0016@maju.edu.pk>
2022-12-17 13:20:30 +0500

    first commit

---
+++ DevopsSeleniumAutomation/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("DevopsSeleniumAutomation")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("DevopsSeleniumAutomation")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("463287ef-e6e5-4e44-a6dd-40391f801e54")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ DevopsSeleniumAutomation/UnitTest1.cs
@@ -0,0 +1,191 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+using System;
+using System.Threading;
+
+namespace DevopsSeleniumAutomation
+{
+    [TestClass]
+    public class UnitTest1
+    {
+            [TestMethod]
+            public void TestCase01_Login()
+            { 
+                WebDriver driver = new ChromeDriver();
+                driver.Manage().Window.Maximize();
+                driver.Url = "https://www.saucedemo.com/";
+                Thread.Sleep(1000);
+                driver.FindElement(By.Id("user-name")).SendKeys("standard_user");
+                driver.FindElement(By.Id("password")).SendKeys("secret_sauce");
+                driver.FindElement(By.Id("login-button")).Click();
+                Thread.Sleep(1000);
+                driver.Close();
+            }
+
+            [TestMethod]
+            public void TestCase02_AddProduct()
+            {
+                WebDriver driver = new ChromeDriver();
+                Thread.Sleep(1000);
+                driver.Manage().Window.Maximize();
+                driver.Url = "https://www.saucedemo.com/";
+                Thread.Sleep(1000);
+                driver.FindElement(By.Id("user-name")).SendKeys("standard_user");
+                driver.FindElement(By.Id("password")).SendKeys("secret_sauce");
+                driver.FindElement(By.Id("login-button")).Click();
+                Thread.Sleep(1000);
+                driver.FindElement(By.Id("add-to-cart-sauce-labs-backpack")).Click();
+                driver.FindElement(By.Id("add-to-cart-sauce-labs-bike-light")).Click();
+                Thread.Sleep(1000);
+              
+                //scroll down
+                IJavaScriptExecutor js = (IJavaScriptExecutor)driver;
+                js.ExecuteScript("window.scrollBy(0,300)");
+                driver.FindElement(By.Id("add-to-cart-sauce-labs-bolt-t-shirt")).Click();
+                driver.FindElement(By.Id("add-to-cart-sauce-labs-fleece-jacket")).Click();
+                js.ExecuteScript("window.scrollBy(0,200)");
+                driver.FindElement(By.Id("add-to-cart-sauce-labs-onesie")).Click();
+                driver.FindElement(By.Id("add-to-cart-test.allthethings()-t-shirt-(red)")).Click();
+                Thread.Sleep(500);
+                js.ExecuteScript("window.scrollBy(0,100)");
+                Thread.Sleep(1000);
+                driver.Close();
+            }
+
+            [TestMethod]
+            public void TestCase03_AddtoCart()
+            {
+                WebDriver driver = new ChromeDriver();
+                Thread.Sleep(1000);
+                //WebDriver driver = new ChromeDriver();
+                driver.Manage().Window.Maximize();
+                driver.Url = "https://www.saucedemo.com/";
+                Thread.Sleep(1000);
+                driver.FindElement(By.Id("user-name")).SendKeys("standard_user");
+                driver.FindElement(By.Id("password")).SendKeys("secret_sauce");
+                driver.FindElement(By.Id("login-button")).Click();
+                Thread.Sleep(1000);
+                driver.FindElement(By.Id("add-to-cart-sauce-labs-backpack")).Click();
+                driver.FindElement(By.Id("add-to-cart-sauce-labs-bike-light")).Click();
+                Thread.Sleep(1000);
+
+                //scroll down
+                IJavaScriptExecutor js = (IJavaScriptExecutor)driver;
+                js.ExecuteScript("window.scrollBy(0,300)");
+                driver.FindElement(By.Id("add-to-cart-sauce-labs-bolt-t-shirt")).Click();
+                driver.FindElement(By.Id("add-to-cart-sauce-labs-fleece-jacket")).Click();
+                js.ExecuteScript("window.scrollBy(0,200)");
+                driver.FindElement(By.Id("add-to-cart-sauce-labs-onesie")).Click();
+                driver.FindElement(By.Id("add-to-cart-test.allthethings()-t-shirt-(red)")).Click();
+                Thread.Sleep(500);
+                js.ExecuteScript("window.scrollBy(0,100)");
+                Thread.Sleep(1000);
+                driver.FindElement(By.XPath("//*[@id=\"shopping_cart_container\"]/a")).Click();
+                Thread.Sleep(1000);
+                driver.FindElement(By.Id("remove-sauce-labs-backpack")).Click();  //remove item
+                Thread.Sleep(1000);
+                driver.FindElement(By.Id("checkout")).Click();
+                Thread.Sleep(1000);
+                driver.FindElement(By.Id("first-name")).SendKeys("Hassan Ali");
+                driver.FindElement(By.Id("last-name")).SendKeys("Gohar");
+                driver.FindElement(By.Id("postal-code")).SendKeys("75400");
+                driver.FindElement(By.Id("continue")).Click();
+                Thread.Sleep(1000);
+                driver.FindElement(By.Id("finish")).Click();
+                string ExpectedResult = driver.FindElement(By.XPath("//*[@id=\"checkout_complete_container\"]/h2")).Text;
+                Assert.AreEqual(ExpectedResult, "THANK YOU FOR YOUR ORDER");
+                driver.Close();
+            }
+
+            [TestMethod]
+            public void TestCase04_NavigationBar()
+            {
+                WebDriver driver = new ChromeDriver();
+                Thread.Sleep(1000);
+                //WebDriver driver = new ChromeDriver();
+                driver.Manage().Window.Maximize();
+                driver.Url = "https://www.saucedemo.com/";
+                Thread.Sleep(1000);
+                driver.FindElement(By.Id("user-name")).SendKeys("standard_user");
+                driver.FindElement(By.Id("password")).SendKeys("secret_sauce");
+                driver.FindElement(By.Id("login-button")).Click();
+                Thread.Sleep(1000);
+                driver.FindElement(By.Id("react-burger-menu-btn")).Click();
+                Thread.Sleep(3000);
+                driver.FindElement(By.Id("logout_sidebar_link")).Click();
+                driver.Close();
+            }
+            [TestMethod]
+            public void TestCase05_AboutScreen()
+            {
+                WebDriver driver = new ChromeDriver();
+                Thread.Sleep(1000);
+                //WebDriver driver = new ChromeDriver();
+                driver.Manage().Window.Maximize();
+                driver.Url = "https://www.saucedemo.com/";
+                Thread.Sleep(1000);
+                driver.FindElement(By.Id("user-name")).SendKeys("standard_user");
+                driver.FindElement(By.Id("password")).SendKeys("secret_sauce");
+                driver.FindElement(By.Id("login-button")).Click();
+                Thread.Sleep(1000);
+                driver.FindElement(By.Id("react-burger-menu-btn")).Click();
+                Thread.Sleep(3000);
+                driver.FindElement(By.Id("logout_sidebar_link")).Click();
+                driver.Close();
+            }
+
+
+
+            [TestMethod]
+            public void TestCase06_ClickOnSoicalIcon()
+            {
+                WebDriver driver = new ChromeDriver();
+                //WebDriver driver = new ChromeDriver();
+                driver.Manage().Window.Maximize();
+                driver.Url = "https://www.saucedemo.com/";
+                Thread.Sleep(1000);
+                driver.FindElement(By.Id("user-name")).SendKeys("standard_user");
+                driver.FindElement(By.Id("password")).SendKeys("secret_sauce");
+                driver.FindElement(By.Id("login-button")).Click();
+                Thread.Sleep(1000);
+
+                IJavaScriptExecutor js = (IJavaScriptExecutor)driver;
+                js.ExecuteScript("window.scrollBy(0,800)");
+                Thread.Sleep(1000);
+                driver.FindElement(By.ClassName("social_twitter")).Click();
+                Thread.Sleep(1000);
+                driver.FindElement(By.ClassName("social_facebook")).Click();
+                Thread.Sleep(1000);
+                driver.FindElement(By.ClassName("social_linkedin")).Click();
+                Thread.Sleep(1000);
+                driver.Close();
+                driver.Quit();
+
+            }
+             [TestMethod]
+            public void TestCase07_ClickOnAboutScreen()
+            {
+            WebDriver driver = new ChromeDriver();
+                //WebDriver driver = new ChromeDriver();
+                driver.Manage().Window.Maximize();
+                driver.Url = "https://www.saucedemo.com/";
+                Thread.Sleep(1000);
+                driver.FindElement(By.Id("user-name")).SendKeys("standard_user");
+                driver.FindElement(By.Id("password")).SendKeys("secret_sauce");
+                driver.FindElement(By.Id("login-button")).Click();
+                Thread.Sleep(1000);
+                Thread.Sleep(1000);
+                driver.FindElement(By.Id("react-burger-menu-btn")).Click();
+                Thread.Sleep(3000);
+                driver.FindElement(By.Id("about_sidebar_link")).Click();
+                driver.Close();
+            }
+        
+
+             
+
+
+
+    }
+}
